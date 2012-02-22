c0c7269da2792e9684ebf3591de83e4295e9e324
KahWee Teng <t@kw.sg>
2012-02-22 18:28:43 +0800

    The actual db session class.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require('LocalSessionHandler.php');
+$session_handler = new LocalSessionHandler();
+session_start();
+$_SESSION['User'] = array('username' => 'kahwee', 'country_id' => 'US', 'first_name' => 'Kah Wee', 'last_name' => 'Teng');
+$_SESSION['User']['country_id'] = 'US';
+$_SESSION['User']['start_ip'] = '';
+unset($_SESSION['User']['first_name']);
+unset($_SESSION['User']['last_name']);
+var_dump($_SESSION);
+?>
